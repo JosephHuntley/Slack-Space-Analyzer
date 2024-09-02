2cd660f65ce38b66c55523752fa123e98e9d14a5
Joseph Huntley <Joseph.Huntley@outlook.com>
2024-09-01 23:46:36 -0400

    Added cmd arguments

---
+++ cmd_arguments.py
@@ -0,0 +1,18 @@
+import argparse
+
+# Create the parser
+parser = argparse.ArgumentParser(description="Process some flags.")
+
+# Add command-line flags (or arguments)
+parser.add_argument('-a', '--analyze', action='store_true', help="Enable analyze mode")
+parser.add_argument('-f', '--file', action='store', type=str, help="File path to slack space")
+parser.add_argument('-p', '--port', action='store', type=str, help="port number to access server")
+parser.add_argument('-s', '--server', action='store_true', help="Make the file data accessible via server")
+parser.add_argument('-e', '--email', action='store', type=str, help="Email to alert the user when operation is complete.")
+parser.add_argument('-l', '--logging', action='store', type=str, help="Log level")
+
+
+
+
+# Parse the arguments
+args = parser.parse_args()
--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 from db import open_db, read_files_db 
+from cmd_arguments import args
 
-import argparse
-
-# Create the parser
-parser = argparse.ArgumentParser(description="Process some flags.")
-
-# Add command-line flags (or arguments)
-parser.add_argument('-a', '--analyze', action='store_true', help="Enable analyze mode")
-parser.add_argument('-f', '--file', action='store', type=str, help="File path to slack space")
-parser.add_argument('-p', '--port', action='store', type=str, help="port number to access server")
-parser.add_argument('-s', '--server', action='store_true', type=str, help="Make the file data accessible via server")
-
-# Parse the arguments
-args = parser.parse_args()
 
 # Handle the arguments
 if args.analyze:
@@ -20,5 +8,7 @@ if args.analyze:
 if args.server:
     print("HOSTING")
 
+conn = open_db()
+read_files_db(conn)
 
 
