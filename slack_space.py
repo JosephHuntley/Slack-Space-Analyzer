f295878d37a34916c9377015fd4106edb283fa5a
Joseph Huntley <Joseph.Huntley@outlook.com>
2024-09-02 11:57:08 -0400

    Added main functions comments. And send email logic

--- slack_space.py
+++ slack_space.py
@@ -1,4 +1,21 @@
+from cmd_arguments import args
+from alert import send_email
+
 
 def analyze_slack(file):
-    # 
+    print("ANALYZING")
+    # Open File
+
+    # Window through binary
+
+    # Regex file header signatures
+
+    # Compare with known signatures
+
+    # Create file obj
+
+    # Add file to database
     
+    # Alert user if -e flag is used
+    if(args.email):
+        send_email()
