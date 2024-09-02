93a470c498d82808b40efcb9f54e6d8872419e72
Joseph Huntley <Joseph.Huntley@outlook.com>
2024-09-02 11:56:41 -0400

    Added actual method when -a flag is present

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from db import open_db, read_files_db 
 from cmd_arguments import args
 from alert import send_email
+from slack_space import analyze_slack
 
+# For dev purposes
+file = None
 
 # Handle the arguments
 if args.analyze:
-    print("ANALYZING")
+    analyze_slack(file)
 if args.server:
     print("HOSTING")
